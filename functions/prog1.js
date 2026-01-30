e321a1e27d99c467abdad6da8b5b03301a9302e2
Ajaykhule143 <ajkhule11@gmail.com>
2026-01-30 15:30:41 +0530

    Initial commit - testing project

---
+++ functions/prog1.js
@@ -0,0 +1,6 @@
+ function adition(a, b) {
+    return a + b;
+}
+//let values = adition(10,20);
+//console.log(values);
+console.log(adition(10, 20));
---
+++ prog2.js
@@ -0,0 +1,9 @@
+function student(){
+    let score = 90;
+    let b="a++"
+    console.log(score + " " + b);
+}
+student();  
+
+// in js no static or dinamic function type
+// we can call function before or after its declaration 
